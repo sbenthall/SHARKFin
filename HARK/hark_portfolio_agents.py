69f6bc216947c64ee01f779055201c90726e9c8d
sb <sbenthall@gmail.com>
2021-04-29 11:13:52 -0400

    consolidate PNL interface into one class; simplify normalized starting resources

--- HARK/hark_portfolio_agents.py
+++ HARK/hark_portfolio_agents.py
@@ -133,23 +133,23 @@ def init_simulations(agents):
         agent.T_sim = 1000 # arbitrary!
         agent.solve()
 
-        ### make and equivalent PF model and solve it
+        ### TODO: make and equivalent PF model and solve it
         ### to get the steady-state wealth
-
-        pf_clone = cism.PerfForesightConsumerType(**agent.parameters)
-        pf_clone.assign_parameters(Rfree = pf_clone.parameters['RiskyAvg'])
-        pf_clone.solve()
+        ### This code block is not working.
+        #pf_clone = cism.PerfForesightConsumerType(**agent.parameters)
+        #pf_clone.assign_parameters(Rfree = pf_clone.parameters['RiskyAvg'])
+        #pf_clone.solve()
+        #
+        #if not hasattr(pf_clone.solution[0],'mNrmStE'):
+        #    # See https://github.com/econ-ark/HARK/issues/1005
+        #    x = 0.7
+        #    print(f"Agent has no steady state normalized market resources. Using {x} as stopgap.")
+        #    pf_clone.solution[0].mNrmStE = x # A hack.
 
         agent.initialize_sim()
 
-        if not hasattr(pf_clone.solution[0],'mNrmStE'):
-            # See https://github.com/econ-ark/HARK/issues/1005
-            x = 0.7
-            print(f"Agent has no steady state normalized market resources. Using {x} as stopgap.")
-            pf_clone.solution[0].mNrmStE = x # A hack.
-
         # set normalize assets to steady state market resources.
-        agent.state_now['mNrm'][:] = pf_clone.solution[0].mNrmStE
+        agent.state_now['mNrm'][:] = 1.0 #pf_clone.solution[0].mNrmStE
         agent.state_now['aNrm'] = agent.state_now['mNrm'] - agent.solution[0].cFuncAdj(agent.state_now['mNrm'])
         agent.state_now['aLvl'] = agent.state_now['aNrm'] * agent.state_now['pLvl']
 
@@ -225,52 +225,106 @@ def combine_lognormal_rates(ror1, std1, ror2, std2):
 #   PNL Interface methods
 ######
 
-import os
+class MarketPNL():
+    netlogo_ror = -0.00052125
+    netlogo_std =  0.0068
+    simulation_price_scale = 0.25
+    default_sim_price = 400
 
-def run_market(config, buy_sell, seed = None):    
-    """
-    Runs the NetLogo market simulation with a given
-    configuration (config), a tuple with the quantities
-    for the brokers to buy/sell (buy_sell), and
-    optionally a random seed (seed)
-    """
-    pnl.run_NLsims(
-        config,
-        broker_buy_limit = buy_sell[0],
-        broker_sell_limit = buy_sell[1],
-        SEED = seed,
-        use_cache = True
-    )
+    # Empirical data -- redundant with FinanceModel!
+    sp500_ror = 0.000628
+    sp500_std = 0.011988
 
-def get_transactions(config, seed = 0, buy_sell = (0,0)):
-    """
-    Given a PNL configuration (config), a random seed (seed)
-    and a tuple of buy/sell (buy_sell), look up the transactions
-    from the associated output file and return it as a pandas DataFrame.
-    """
-    logfile = pnl.transaction_file_name(config, seed, buy_sell[0], buy_sell[1])
+    #
+    last_buy_sell = None
+    last_seed = None
+
+    def __init__(
+        self,
+        config_file = "../PNL/macroliquidity.ini",
+        config_local_file = "../PNL/macroliquidity_local.ini"
+    ):
+        self.config = UTIL.read_config(
+            config_file = config_file,
+            config_local_file = config_local_file
+        )
     
-    # use run_market() first to create logs
-    transactions = pd.read_csv(
-        logfile,
-        delimiter='\t'
-    )
-    return transactions
+    def run_market(self, seed = 0, buy_sell = 0):
+        """
+        Runs the NetLogo market simulation with a given
+        configuration (config), a tuple with the quantities
+        for the brokers to buy/sell (buy_sell), and
+        optionally a random seed (seed)
+        """
+        self.last_seed = seed
+        self.last_buy_sell = buy_sell
 
-def get_last_simulation_price(config, seed = 0, buy_sell = (0,0)):
-    """
-    Get the last price from the simulation.
-    Returns None if the transaction file was empty for some reason.
+        pnl.run_NLsims(
+            self.config,
+            broker_buy_limit = buy_sell[0],
+            broker_sell_limit = buy_sell[1],
+            SEED = seed,
+            use_cache = True
+        )
 
-    TODO: Better docstring
-    """
+    def get_transactions(self, seed = 0, buy_sell = (0,0)):
+        """
+        Given a random seed (seed)
+        and a tuple of buy/sell (buy_sell), look up the transactions
+        from the associated output file and return it as a pandas DataFrame.
+        """
+        logfile = pnl.transaction_file_name(
+            self.config,
+            seed,
+            buy_sell[0],
+            buy_sell[1]
+        )
 
-    transactions = get_transactions(config, seed=seed, buy_sell = buy_sell)
-    prices = transactions['TrdPrice']
+        # use run_market() first to create logs
+        transactions = pd.read_csv(
+            logfile,
+            delimiter='\t'
+        )
+        return transactions
 
-    if len(prices.index) == 0:
-        ## BUG FIX HACK
-        print("ERROR in PNL script: zero transactions. Reporting no change")
-        return None
+    def get_simulation_price(self, seed = 0, buy_sell = (0,0)):
+        """
+        Get the price from the simulation run.
+        Returns None if the transaction file was empty for some reason.
 
-    return prices[prices.index.values[-1]]
+        TODO: Better docstring
+        """
+
+        transactions = self.get_transactions(seed=seed, buy_sell = buy_sell)
+        prices = transactions['TrdPrice']
+
+        if len(prices.index) == 0:
+            ## BUG FIX HACK
+            print("ERROR in PNL script: zero transactions. Reporting no change")
+            return None
+
+        return prices[prices.index.values[-1]]
+
+
+    def daily_rate_of_return(self, seed = None, buy_sell = None):
+        ## TODO: Cleanup. Use "last" parameter in just one place.
+        if seed is None:
+            seed = self.last_seed
+
+        if buy_sell is None:
+            buy_sell = self.last_buy_sell
+
+        last_sim_price = self.get_simulation_price(
+            seed=seed, buy_sell = buy_sell
+        )
+
+        if last_sim_price is None:
+            last_sim_price = self.default_sim_price
+
+        ror = (last_sim_price * self.simulation_price_scale - 100) / 100
+
+        # adjust to calibrated NetLogo to S&P500
+        ror = self.sp500_std * (ror - self.netlogo_ror) / self.netlogo_std + self.sp500_ror
+
+        return ror
+
