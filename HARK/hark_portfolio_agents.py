7b773296fa06505bbb3e886b08c453c940c9ae25
sb <sbenthall@gmail.com>
2021-05-03 17:06:32 -0400

    Consumers sell shares to financial quarterly consumption in the macro update step

--- HARK/hark_portfolio_agents.py
+++ HARK/hark_portfolio_agents.py
@@ -65,7 +65,7 @@ def initialize_agents(agent_classes, agent_parameters):
         for ac
         in agent_classes
     ]
-    
+
     return agents
 
 def distribute_beta(agents):
@@ -219,6 +219,139 @@ def combine_lognormal_rates(ror1, std1, ror2, std2):
     sigma3 = math.sqrt((math.exp(var3) - 1) * math.exp(2 * mu3 + var3))
 
     return ror3, sigma3
+
+######
+#   Model classes used in the simulation
+######
+
+class FinanceModel():
+    """
+    A class representing the financial system in the simulation.
+
+    Contains parameter values for:
+      - the capital gains expectations function
+      - the dividend
+      - the risky asset expectations
+      
+    Contains data structures for tracking ROR and STD over time.
+    """
+    # Empirical data
+    sp500_ror = 0.000628
+    sp500_std = 0.011988
+    
+    # Simulation parameter
+    days_per_quarter = 60
+    
+    # Expectation calculation parameters    
+    p1 = 0.1
+    delta_t1 = 30
+
+    a = - math.log(p1) / delta_t1
+
+    p2 = 0.1
+    delta_t2 = 60
+
+    b = math.log(p2) / delta_t2
+
+    # Quarterly dividend rate and standard deviation.
+    dividend_ror = 0.03
+    dividend_std = 0.01
+
+    # Data structures. These will change over time.
+    starting_price = 100
+    prices = [starting_price]
+    ror_list = []
+
+    expected_ror_list = []
+    expected_std_list = []
+
+    def add_ror(self, ror):
+        self.ror_list.append(ror)
+        asset_price = self.prices[-1] * (1 + ror)
+        self.prices.append(asset_price)
+        return asset_price
+
+    def __init__(self, dividend_ror = None, dividend_std = None):
+
+        if dividend_ror:
+            self.dividend_ror = divedend_ror
+
+        if dividend_std:
+            self.dividend_std = dividend_std
+        pass
+
+    def calculate_risky_expectations(self):
+        """
+        Compute the quarterly expectations for the risky asset based on historical return rates.
+
+        In this implementation there are a number of references to:
+          - paramters that are out of scope
+          - data structures that are out of scope
+
+        These should be bundled together somehow.
+
+        NOTE: This MUTATES the 'expected_ror_list' and so in current design
+        has to be called on a schedule... this should be fixed.
+        """
+        # note use of data store lists for time tracking here -- not ideal
+        D_t = sum([math.exp(self.a * (l + 1)) for l in range(len(self.ror_list))])
+        S_t = math.exp(self.b * (len(self.prices) - 1)) # because p_0 is included in this list.
+
+        w_0 = S_t
+        w_t = [(1 - S_t) * math.exp(self.a * (t+1)) / D_t for t in range(len(self.ror_list))]
+
+        print(f"D_t: {D_t}\nS_t / w_0  : {S_t}") # "\nror_list: {ror_list}\n w_t: {w_t}")
+    
+        expected_ror = w_0 * self.sp500_ror + sum(
+            [w_ror[0] * w_ror[1]
+             for w_ror
+             in zip(w_t, self.ror_list)])
+        self.expected_ror_list.append(expected_ror)
+
+        expected_std = math.sqrt(
+            w_0 * pow(self.sp500_std, 2) \
+            +  sum([w_ror_er[0] * pow(w_ror_er[1] - expected_ror, 2)
+                    for w_ror_er
+                    in zip(w_t, self.ror_list)]))
+        self.expected_std_list.append(expected_std)
+
+        print(f'daily expectations: {expected_ror},{expected_std}')
+
+    def risky_expectations(self):
+        """
+        Return quarterly expectations for the risky asset.
+        These are the average and standard deviation for the asset
+        including both capital gains and dividends.
+        """
+        # expected capital gains quarterly
+        ex_cg_q_ror = ror_quarterly(self.expected_ror_list[-1], self.days_per_quarter)
+        ex_cg_q_std = sig_quarterly(self.expected_std_list[-1], self.days_per_quarter)
+
+        # factor in dividend:
+        cg_w_div_ror, cg_w_div_std = combine_lognormal_rates(
+            ex_cg_q_ror,
+            ex_cg_q_std,
+            self.dividend_ror,
+            self.dividend_std
+        )
+
+        market_risky_params = {
+            'RiskyAvg': 1 + cg_w_div_ror,
+            'RiskyStd': cg_w_div_std
+        }
+
+        return market_risky_params
+
+    def reset(self):
+        """
+        Reset the data stores back to original value.
+        """
+        self.prices = [100]
+        self.ror_list = []
+
+        self.expected_ror_list = []
+        self.expected_std_list = []
+
 
 
 ######
