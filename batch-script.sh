751dbd1673f0c5d7eb46e4ee3ec5f86da7261dcc
sb <sbenthall@gmail.com>
2021-08-12 16:12:50 -0400

    correcting command line arguments to batch-script.sh

--- batch-script.sh
+++ batch-script.sh
@@ -2,4 +2,4 @@
 # Proper header for a Bash script.
 pip install -r /home/sb/HARK_ABM_INTRO_public/HARK/requirements.txt
 cd /home/sb/HARK_ABM_INTRO_public/HARK
-python3 run_simulations.py $1 echo ${2:+"--tag $2"}
+python3 run_simulations.py $1 ${2:+"--tag $2"}
