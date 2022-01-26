12da868006c3dc514dc415728a15d298623ec7e8
Nick Sawhney <nicksawhney@gmail.com>
2022-01-26 15:54:03 -0800

    conform to market API

--- HARK/test_rpc.py
+++ HARK/test_rpc.py
@@ -2,5 +2,9 @@ import hark_portfolio_agents as hpa
 	
 market = hpa.ClientRPCMarket()
 
-x = market.run_market(seed=1, buy_sell=(2, 3))
-print(x)
+market.run_market(seed=1, buy_sell=(2, 3))
+
+ror = market.daily_rate_of_return()
+
+print(ror)
+
