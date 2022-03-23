aa32379eba0c090ff92b6ae70dd0f75be1a96ab9
Nick Sawhney <nicksawhney@gmail.com>
2022-03-23 13:22:39 -0700

    break hark_portfolio_agents into separate files

---
+++ sharkfin/broker.py
@@ -0,0 +1,98 @@
+import HARK.ConsumptionSaving.ConsPortfolioModel as cpm
+import HARK.ConsumptionSaving.ConsIndShockModel as cism
+from HARK.core import distribute_params
+from sharkfin.utilities import *
+from datetime import datetime
+from HARK.distribution import Uniform
+import io
+import itertools
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import random
+import seaborn as sns
+from statistics import mean
+from scipy import stats
+import yaml
+import json
+import pika
+import uuid
+import time
+from typing import Tuple
+
+class Broker:
+    """
+    A share Broker. Collects orders from agents, then trades on the market
+    and reports a rate of return.
+
+    Parameters
+    ----------
+    market - a MarketPNL instance
+    """
+
+    buy_limit = 0
+    sell_limit = 0
+
+    # track the buy/sell orders due to macro updates separately
+    # this data won't go to sim_stats yet but will be used for investigating
+    # the mechanism
+    buy_orders_macro = 0
+    sell_orders_macro = 0
+
+    buy_sell_history = None
+    buy_sell_macro_history = None
+
+    market = None
+
+    def __init__(self, market):
+        self.market = market
+        self.buy_sell_history = []
+        self.buy_sell_macro_history = []
+
+    def transact(self, delta_shares, macro=False):
+        """
+        Input: an array of share deltas. positive for buy, negative for sell.
+
+        macro: True if the transactions are from a 'macro update' step.
+        """
+        self.buy_limit += delta_shares[delta_shares > 0].sum()
+        self.sell_limit += -delta_shares[delta_shares < 0].sum()
+
+        if macro:
+            self.buy_orders_macro += delta_shares[delta_shares > 0].sum()
+            self.sell_orders_macro += -delta_shares[delta_shares < 0].sum()
+
+    def trade(self, seed=None):
+        """
+        Broker executes the trade on the financial market and then updates
+        their record of the current asset price.
+
+        Input: (optional) random seed for the simulation
+        Output: Rate of return of the asset value that day.
+        """
+
+        # use integral shares here.
+        buy_sell = (int(self.buy_limit), int(self.sell_limit))
+        self.buy_sell_history.append(buy_sell)
+
+        buy_sell_macro = (int(self.buy_orders_macro), int(self.sell_orders_macro))
+        self.buy_sell_macro_history.append(buy_sell_macro)
+        # print("Buy/Sell Limit: " + str(buy_sell))
+
+        self.market.run_market(buy_sell=buy_sell, seed=seed)
+
+        # clear the local limits
+        self.buy_limit = 0
+        self.sell_limit = 0
+
+        self.buy_orders_macro = 0
+        self.sell_orders_macro = 0
+
+        return buy_sell, self.market.daily_rate_of_return()
+
+    def close(self):
+        self.market.close_market()
+
+        
---
+++ sharkfin/markets/__init__.py
@@ -0,0 +1,99 @@
+class AbstractMarket(ABC):
+    '''
+    Abstract class from which market models should inherit
+
+    defines common methods for all market models.
+    '''
+
+    @abstractmethod
+    def run_market():
+        pass
+
+    @abstractmethod
+    def get_simulation_price(self, seed: int, buy_sell: Tuple[int, int]):
+        # does this need to be an abstract method or can it be encapsulated in daily_rate_of_return?
+        pass
+
+    @abstractmethod
+    def daily_rate_of_return(self, seed: int, buy_sell: Tuple[int, int]):
+        pass
+
+    @abstractmethod
+    def close_market():
+        pass
+        
+
+class MockMarket(AbstractMarket):
+    """
+    A wrapper around the Market PNL model with methods for getting
+    data from recent runs.
+
+    Parameters
+    ----------
+    config_file
+    config_local_file
+
+    """
+
+    # Properties of the PNL market model
+    netlogo_ror = 0.0
+    netlogo_std = 0.025
+
+    simulation_price_scale = 0.25
+    default_sim_price = 400
+
+    # Empirical data -- redundant with FinanceModel!
+    sp500_ror = 0.000628
+    sp500_std = 0.011988
+
+    # Storing the last market arguments used for easy access to most
+    # recent data
+    last_buy_sell = None
+    last_seed = None
+
+    def __init__(self):
+        pass
+
+    def run_market(self, seed=0, buy_sell=0):
+        """
+        Sample from a probability distribution
+        """
+        self.last_seed = seed
+        self.last_buy_sell = buy_sell
+
+    def get_simulation_price(self, seed=0, buy_sell=(0, 0)):
+        """
+        Get the price from the simulation run.
+
+        TODO: Better docstring
+        """
+        mean = 400 + np.log1p(buy_sell[0]) - np.log1p(buy_sell[1])
+        std = 10 + np.log1p(buy_sell[0] + buy_sell[1])
+        price = np.random.normal(mean, std)
+        return price
+
+    def daily_rate_of_return(self, seed=None, buy_sell=None):
+
+        if seed is None:
+            seed = self.last_seed
+
+        if buy_sell is None:
+            buy_sell = self.last_buy_sell
+
+        last_sim_price = self.get_simulation_price(seed=seed, buy_sell=buy_sell)
+
+        if last_sim_price is None:
+            last_sim_price = self.default_sim_price
+
+        ror = (last_sim_price * self.simulation_price_scale - 100) / 100
+
+        # adjust to calibrated NetLogo to S&P500
+        ror = (
+            self.sp500_std * (ror - self.netlogo_ror) / self.netlogo_std
+            + self.sp500_ror
+        )
+
+        return ror
+
+    def close_market(self):
+        return
