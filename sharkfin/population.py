e17b070ebe913680b65dea49a31de74c0057e6ef
Alan Lujan <alanlujan91@gmail.com>
2022-06-22 17:07:43 -0500

    Update population.py
    add solve method

--- sharkfin/population.py
+++ sharkfin/population.py
@@ -270,6 +270,15 @@ class AgentPopulation:
             agent.T_sim = T_sim
             agent.initialize_sim()
 
+    def solve(self, merge_by= None):
+
+        self.solve_distributed_agents()
+
+        if merge_by is not None:
+
+            self.solution = AgentPopulationSolution(self)
+            self.solution.merge_solutions(continuous_states=merge_by)
+
 
 class AgentPopulationSolution:
     def __init__(self, agent_population):
