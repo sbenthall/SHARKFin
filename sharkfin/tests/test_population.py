c1fd17c694bc1a34950fbe116d05609608d013ee
Alan Lujan <alanlujan91@gmail.com>
2022-06-30 10:19:21 -0500

    test again with fewer agents

--- sharkfin/tests/test_population.py
+++ sharkfin/tests/test_population.py
@@ -9,6 +9,8 @@ from simulate.parameters import (
 
 parameter_dict = agent_population_params | continuous_dist_params
 
+parameter_dict["AgentCount"] = 1
+
 
 def test_agent_population():
     # Initializing an Agent Population
@@ -18,39 +20,39 @@ def test_agent_population():
     # ADD PRINT LINE AFTER EVERY STEP
     print("created agent population")
 
-    # # Step 2 - provide approximation parameters
+    # Step 2 - provide approximation parameters
     ap.approx_distributions(approx_params)
     print("approximated continuous distributions")
 
-    # # Step 3 - parse all parameters to create distributed agent parameter dictionaries
+    # Step 3 - parse all parameters to create distributed agent parameter dictionaries
     ap.parse_params()
     print("parsed parameters")
 
-    # # Step 4 - create distributed agents and put them in a list
+    # Step 4 - create distributed agents and put them in a list
     ap.create_distributed_agents()
     print("created distributed agents")
 
-    # # Step 5 - create pandas database of agents for better selection by parameters
+    # Step 5 - create pandas database of agents for better selection by parameters
     ap.create_database()
     print("created agent database")
 
-    # # Step 6 - solve each of the agent groups in the population
+    # Step 6 - solve each of the agent groups in the population
     ap.solve_distributed_agents()
     print("solved distributed agents")
 
-    # # Step 7 - initialize simulation for each agent sub-population
+    # Step 7 - initialize simulation for each agent sub-population
     ap.init_simulation()
     print("initialized simulation")
-    #
-    # # Step 8 - create master solution as attribute of population
-    # # ap.solve(["RiskyAvg", "RiskyStd"])
-    #
-    # # Creating Master Solution for agent Population separately
-    #
-    # # Step 1. create Agent Population Solution (must follow 1-6 above before this)
-    # solution = AgentPopulationSolution(ap)
-    # print("created solution object")
 
-    # # Step 2. provide parameters that will become state variables
-    # solution.merge_solutions(["RiskyAvg", "RiskyStd"])
-    # print("merged solutions")
+    # Step 8 - create master solution as attribute of population
+    # ap.solve(["RiskyAvg", "RiskyStd"])
+
+    # Creating Master Solution for agent Population separately
+
+    # Step 1. create Agent Population Solution (must follow 1-6 above before this)
+    solution = AgentPopulationSolution(ap)
+    print("created solution object")
+
+    # Step 2. provide parameters that will become state variables
+    solution.merge_solutions(["RiskyAvg", "RiskyStd"])
+    print("merged solutions")
